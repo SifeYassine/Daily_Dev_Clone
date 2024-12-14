70dfb6c4b19c46df4cc7b680765fb6125a2b25fb
SifeYassine <sifeddineyassine1@gmail.com>
2024-12-14 19:47:55 +0100

    Initial Setup, Laravel Backend, Next.Js Frontend, Shadcn UI

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      Hello, I'm a Next.js app
+      <Button>Click me!</Button>
+    </div>
+  );
+}
